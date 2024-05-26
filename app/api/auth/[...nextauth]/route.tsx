4cfcc9f4d26434549866fea938734b9e5dfc2159
KiranGaneshKorada <kiranganeshkorada@gmail.com>
2024-05-26 17:56:49 +0530

    done with the functionality of authentication need to connect with the schena complaint and add login logut stuff

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,10 +1,48 @@
 import prisma from "@/prisma/client";
+import bcrypt from "bcrypt";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        student_email: {
+          label: "Student Email",
+          type: "Email",
+          placeholder: "student_id@college_name.edu",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.student_email || !credentials.password) {
+          return null;
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.student_email },
+        });
+
+        if (!user) {
+          return null;
+        }
+
+        const isPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword!
+        );
+
+        if (isPassword) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
 });
 
 export { handler as GET, handler as POST };
