8605f6362c6319cd934f5920a084cd95f8a03a3e
KiranGaneshKorada <kiranganeshkorada@gmail.com>
2024-06-23 13:05:34 +0530

    done with status

--- app/_helper_components/DashBoard_Components/ComplaintsSummary/StatusSummary.tsx
+++ app/_helper_components/DashBoard_Components/ComplaintsSummary/StatusSummary.tsx
@@ -1,122 +1,48 @@
-// 'use client'
-
-// import Link from "next/link";
 import React from "react";
 
 
-const statusCardData=["Closed Complaints", ""]
+interface Props {
+  open: number;
+  inProgress: number;
+  closed: number;
+}
 
-const StatusSummary = () => {
+const statusMap: { [key: string]: string } = {
+  OPEN: "  text-red-800  ",
+  IN_PROGRESS: " text-blue-800  ",
+  CLOSED: " text-teal-800 ",
+};
+
+
+const StatusSummary = ({open,inProgress,closed}:Props) => {
+  const containers: {
+    label: string;
+    value: number;
+    status: string;
+
+  }[] = [
+    { label: 'Open Complaints', value: open, status: 'OPEN' },
+    {
+      label: 'In-progress Complaints',
+      value: inProgress,status: 'IN_PROGRESS',
+    },
+    { label: 'Closed Complaints', value: closed ,status: 'CLOSED'},
+  ];
   return (
     <div className="flex flex-row space-x-2">
-      <div className="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70">
-        <div className="bg-gray-100 border-b rounded-t-xl py-3 px-4 md:py-4 md:px-5 dark:bg-neutral-900 dark:border-neutral-700">
-          <p className="mt-1 text-sm text-gray-500 dark:text-neutral-500">
-            Complaints
+      {containers.map((each_card_data)=>(<div className="flex flex-col bg-white border shadow-sm rounded-xl">
+        <div className="bg-gray-100 border-b rounded-t-xl py-3 px-4 md:py-4 md:px-5">
+          <p className="mt-1 text-md font-semibold text-blue-500">
+            {each_card_data.label}
           </p>
         </div>
         <div className="p-4 md:p-5">
-          <h3 className="text-lg font-bold text-gray-800 dark:text-white">
-            Open
+          <h3 className={`text-lg font-bold ${
+                    statusMap[each_card_data.status]}`}>
+            {each_card_data.value}
           </h3>
-          <p className="mt-2 text-gray-500 dark:text-neutral-400">
-            With supporting text below as a natural lead-in to additional
-            content.
-          </p>
-          {/* <Link
-            className="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400"
-            href="/complaints"
-          >
-            Complaints
-            <svg
-              className="flex-shrink-0 size-4"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path d="m9 18 6-6-6-6"></path>
-            </svg>
-          </Link> */}
         </div>
-      </div>
-      <div className="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70">
-        <div className="bg-gray-100 border-b rounded-t-xl py-3 px-4 md:py-4 md:px-5 dark:bg-neutral-900 dark:border-neutral-700">
-          <p className="mt-1 text-sm text-gray-500 dark:text-neutral-500">
-            Complaints
-          </p>
-        </div>
-        <div className="p-4 md:p-5">
-          <h3 className="text-lg font-bold text-gray-800 dark:text-white">
-            Closed
-          </h3>
-          <p className="mt-2 text-gray-500 dark:text-neutral-400">
-            With supporting text below as a natural lead-in to additional
-            content.
-          </p>
-          {/* <Link
-            className="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400"
-            href="/complaints"
-          >
-            Complaints
-            <svg
-              className="flex-shrink-0 size-4"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path d="m9 18 6-6-6-6"></path>
-            </svg>
-          </Link> */}
-        </div>
-      </div>
-      <div className="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70">
-        <div className="bg-gray-100 border-b rounded-t-xl py-3 px-4 md:py-4 md:px-5 dark:bg-neutral-900 dark:border-neutral-700">
-          <p className="mt-1 text-sm text-gray-500 dark:text-neutral-500">
-            Complaints
-          </p>
-        </div>
-        <div className="p-4 md:p-5">
-          <h3 className="text-lg font-bold text-gray-800 dark:text-white">
-            In-Progress
-          </h3>
-          <p className="mt-2 text-gray-500 dark:text-neutral-400">
-            With supporting text below as a natural lead-in to additional
-            content.
-          </p>
-          {/* <Link
-            className="mt-3 inline-flex items-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400"
-            href="/complaints"
-          >
-            Complaints
-            <svg
-              className="flex-shrink-0 size-4"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path d="m9 18 6-6-6-6"></path>
-            </svg>
-          </Link> */}
-        </div>
-      </div>
+      </div>))}
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,34 @@ import { useSession } from "next-auth/react";
 import LatestComplaints from "./_helper_components/DashBoard_Components/LatestComplaints";
 import StatusSummary from "./_helper_components/DashBoard_Components/ComplaintsSummary/StatusSummary";
 import { Metadata } from "next";
+import prisma from "@/prisma/client";
 
-export default function Home() {
+export default async function Home() {
+
+  const open = await prisma.complaint.count({
+    where: { status: 'OPEN' },
+  });
+  const inProgress = await prisma.complaint.count({
+    where: { status: 'IN_PROGRESS' },
+  });
+  const closed = await prisma.complaint.count({
+    where: { status: 'CLOSED' },
+  });
   // const { data } = useSession();
   // console.log(data);
   return (
     <div className="p-5">
       <div className=" m-2">
-      <StatusSummary />
+      <StatusSummary open={open}
+          inProgress={inProgress}
+          closed={closed}/>
       </div>
       
       <LatestComplaints />
     </div>
   );
 }
+export const dynamic = 'force-dynamic'; 
 
 
 export const metadata: Metadata = {
